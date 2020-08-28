13a76a1e932af919d6d5a26b2b5be5eaafa05d06
shadorized <54058312+shadorized@users.noreply.github.com>
2020-08-28 19:16:43 +0100

    update

--- Climate Project/testingcode.py
+++ Climate Project/testingcode.py
@@ -7,11 +7,13 @@ import decimal
 import os
 import numpy as np
 import copy
+import scipy.stats as sp
+
 
 filename= 'TRMM1/3B42_Daily.19980101.7.nc4.nc4'
 f=netCDF4.Dataset(filename)
 lat, lon= [f.variables['lat'][:], f.variables['lon'][:]] #13 longitudes, 11 latitudes
-
+#print(lat, lon)
 
 
 class ReadData:
@@ -25,7 +27,8 @@ class ReadData:
     sortedKeys_DMI = []
     TRMM_data = {}
     TRMM_list = []
-    TRMM_mod7 = 0
+    TRMM_acc_list = [] #weekly precepitation
+    TRMM_mod7 = 6 #this is the only way to get the data to match up with NINO and DMI
     sortedKeys_TRMM = []
     ElNino_data= {}
     ElNino_list = [[],[],[],[]]
@@ -33,8 +36,9 @@ class ReadData:
     data_dict = {}
     
     #constants
-    begin = datetime.date(1998, 1, 1) #begin of dates we look at
-    end = datetime.date(2019, 12, 30) #end of dates we look at
+    begin = datetime.date(1998, 1, 1) #begin of dates we look at. We need to start a bit late so we can get 7 previous days to use for acc
+    end = datetime.date(2019, 12, 25) #end of dates we look at
+    #note that we have to discard this final point to make everything even
     
     
     
@@ -45,7 +49,6 @@ class ReadData:
         
         self.DMI_data = self.generateDataDMI()
         print("DMI data generated")
-        self.findMinData(self.DMI_data)
         self.sortedKeys_DMI = self.sortKeys(self.DMI_data)   
         for i in self.sortedKeys_DMI:
             self.DMI_list.append(self.DMI_data[i])
@@ -56,7 +59,8 @@ class ReadData:
             self.TRMM_list.append(self.TRMM_data[i])
         print("TRMM data generated")
         sum_L = self.accumulateTRMM()
-        mod7_0 = self.takeModN_TRMM(sum_L) #this creates a list with the summed precipitation fo the previous days
+        self.TRMM_acc_list = self.takeModN_TRMM(sum_L) #this creates a list with the summed precipitation fo the previous days
+        
         #and we only take days that are mod7 == 0
         #
         
@@ -71,17 +75,24 @@ class ReadData:
         print("ELNino data generated")
         
     def accumulateTRMM(self):
-        TRMM_list_update = []
+        TRMM_list_update = [] #because we have to skip first date, we lose one data point
+        v_0 = 0
+        v_1 = 0
         for i in range(6, len(self.TRMM_list)):
+            v_0 += self.TRMM_list[i][0][0]
+            v_1 += self.TRMM_list[i][3][3]
             TRMM_list_update.append(copy.deepcopy(self.TRMM_list[i]) ) #arrays are pass by REFRENCE--need to copy.
             for j in range(0,6):
                 TRMM_list_update[i-6] += self.TRMM_list[i-j-1]
+        #print(v_0)
+        #print(v_1)
         return TRMM_list_update
     def takeModN_TRMM(self,L):
         TRMM_list_res = []
         for i in range(0,len(L)):
             if(i % 7 == self.TRMM_mod7): #only take number with mod of TRMM_mod7
                 TRMM_list_res.append(L[i])
+                #look at this sorted key, why are we missing the last key?
             
         return TRMM_list_res
     
@@ -95,7 +106,7 @@ class ReadData:
             if(v == 0 or v> diff):
                 v = diff
         #print(v)
-        return v
+        #return v
         
         
     
@@ -119,12 +130,17 @@ class ReadData:
             DMIdate.append(offset)
 
         date_DMI={}
-        for j in range(0, len(SSTA)):
+        skip = True
+        for j in range(0, len(SSTA)): #skip first data point, so we can meet with TRMM
             if(self.isInTimeRange(DMIdate[j])):
-                date_DMI[str(DMIdate[j]).replace("-","")] =  SSTA[j]
+                if(not skip):
+                    date_DMI[str(DMIdate[j]).replace("-","")] =  SSTA[j]
+                else:
+                    skip = False #skip one data point
             else:
                 pass
                 #this data we will just discard for now
+        #print("len",len(date_DMI)) 1146 length, since we skip one data point
         return date_DMI
     
     def generateDataElNino(self):
@@ -136,14 +152,20 @@ class ReadData:
         #12,3,34,4 are the ninos we take
         
         search_range = [[19,23],[32,36],[45,49],[58,62]]
+        
         for j in range(0,len(search_range)):
-            for i in range(4, len(linelist)):
+            skip = True
+            for i in range(4, len(linelist)): #skip the first datapoint
                 date_str = self.replaceDateTime(linelist[i][1:10])
                 if (self.isInTimeRangeStr(date_str)):
-                    date_Ninos[j][date_str] =  float(linelist[i][search_range[j][0]:search_range[j][1]])            
+                    if(not skip):
+                        date_Ninos[j][date_str] =  float(linelist[i][search_range[j][0]:search_range[j][1]])            
+                    else:
+                        skip = False
                 else:
                     pass
                     #this is the date we discard
+            print(len(date_Ninos[j]) )  #is 1146, since we skip one point
         f.close() 
 
         
@@ -159,6 +181,7 @@ class ReadData:
             if filename.endswith('nc4'):
                 f=netCDF4.Dataset(TRMMfolder + str(filename))
                 precp=f.variables['precipitation'][:]
+                
                 date_str = str(filename[11:19])
                 #if(self.isInTimeRangeStr(date_str) and not str(self.toDateTime(date_str)-self.begin)=="0:00:00" and int(str(self.toDateTime(date_str)-self.begin).split(" ")[0]) % 7 == self.TRMM_mod7  ):
                 if(self.isInTimeRangeStr(date_str)):    
@@ -184,9 +207,7 @@ class ReadData:
     def replaceDateTime(self,string):
         return str(datetime.datetime.strptime(string, '%d%b%Y')).split(" ")[0].replace("-","")
     
-
-
-
+    
 
 reader = ReadData()
 
@@ -194,52 +215,99 @@ reader = ReadData()
 class AnalyzeData:
     "class for anaylzing the data we generated from a reader"
     reader_class = None
-    snhtsetDMI = []
-    snhtsetNino = []
+    snhtsetTRMM = []
+    mx_lon = 13
+    mx_lat = 11
     def __init__(self,reader):
         self.reader_class = reader
-        self.generateAllSNHT()
+        #self.generateAllSNHT()
     def generateAllSNHT(self):
-        self.snhtsetDMI = self.SNHT(reader.DMI_list)
-        #snhtsetTRMM = self.SNHT(reader.TRMM_list) 
-        #^^ we can't use this set, since we have more than one varaible. 
-        #QUESTION: how should we deal with this set?   ## Run through a "multidimensional test". 
-        self.snhtsetNino = []
-        for i in reader.ElNino_list:    
-            self.snhtsetNino.append(self.SNHT(i))
+        self.snhtsetTRMM= [] #the snht set is ordered as a list (lon,lat,?)
+        for lon in range(0,self.mx_lon):
+            lat_list = []
+            for lat in range(0,self.mx_lat):
+                #print("trmm",type(self.getDatasetTRMM(lon,lat)[0]))
+                lat_list.append(self.SNHTALL(self.getDatasetTRMM(lon,lat)))
+                pass
+            self.snhtsetTRMM.append(lat_list)
+        
         #TODO: continue with taking the snhtset
-    def SNHT(self,dataset):   #SNHT FOR time series of dimension 1 x n
-        #This function is fairly slow, but we only need to run it once, so speed isn't all that important
-        n=len(dataset)  
-        sd=decimal.Decimal(stats.pstdev(dataset))
-        mean = decimal.Decimal(stats.mean(dataset))
+    def getDatasetTRMM(self,lon,lat):
+        #right now, we don't check to ensure the list is correctly sized, add that later
+        #i,j means for each item, take the ith item, and the jth item from that list, and form it into a dataset
+        list_res = []
+        
+        for item in self.reader_class.TRMM_acc_list:
+            list_res.append(item[lon][lat])
+        
+        return list_res
+    #(Old snht test)
+    
+    def SNHTALL(self,dataset):   #SNHT FOR time series of dimension 1 x n
+        n=len(dataset)
+        sd=np.std(dataset)
+        mean = stats.mean(dataset)
+        
         snhtset=[]
+        mx_v = 0
         for y in range(1,n-1): #Loops from Week 1 to week n-1
             summ1=0 
-            for i in range(1, y+1): #QUESTION: do we want to skip the first week? Why not include 0?
-                summ1 += decimal.Decimal((decimal.Decimal(dataset[i]) - mean)/sd)
-            z_1= decimal.Decimal(1/(y+1)) * summ1
+            for i in range(0, y): 
+                summ1 += dataset[i]-mean
+            z_1= (1.0/(y*sd)) * (summ1)
             summ2=0
-            for i in range(y+1, n):
-                summ2 += decimal.Decimal((decimal.Decimal(dataset[i]) - mean)/sd)
-            z_2=decimal.Decimal(1.0/(n-y)) *summ2
-            snhtset.append(y * (z_1 **2) + (n-y)*(z_2**2))
-        return snhtset
+            for i in range(y, n):
+                summ2 += dataset[i]-mean
+            z_2= (1/( (n-y)*sd)) * (summ2)
+            v = y * (z_1 **2) + (n-y)*(z_2**2)
+            if(v > mx_v):
+                mx_v = v
+            if(v > 9.95):
+                print(v)
+                print(y)
+            snhtset.append(v)
+        
+        return (snhtset,mx_v)
+    
 
-def Pearlson(x, y, lag):      #LAG means that we compare x_t against y_{t-lag} where lag is positive
-    x=x[lag:]
-    y=y[:-lag]
-    x_mean=stats.mean(x)
-    y_mean=stats.mean(y)
-    top=0
-    n=len(x)-lag
-    for j in range(0, n):
-        top=top+(x[j]-x_mean)*(y[j]-y_mean)
-    pearlson= top/(n*stats.stdev(x) * stats.stdev(y))   
-    return pearlson
-a=reader.DMI_list
-b=reader.ElNino_list[0]
-t=reader.DMI_data.keys()
-plt.plot(t, a, 'r', t, b, 'g')
-plt.show()
+
+    def Pearson(self,x, y, lag): #LAG means that we compare x_t against y_{t-lag} where lag is positive
+        if int(lag)!=0:
+            x=x[lag:]
+            y=y[:-lag]
+        else:
+            pass
+        cor, pval=sp.pearsonr(x, y)
+        return [lag, cor, pval]
+analyze = AnalyzeData(reader) 
+
+b=reader.DMI_list[:]
+pearlarray=[]
+for k in range(0, 13):
+    lat=[]
+    for i in range(0, 11):
+        a=[reader.TRMM_acc_list[j][k][i]/100 for j in range(0, 1146)]
+        current=analyze.Pearson(a, b, 0)
+        for l in range(0, 52):
+            new=analyze.Pearson(a,b,l)
+            if float(new[1])>float(current[1]):
+                current=analyze.Pearson(a,b,l)
+            else:
+                pass
+        lat.append([current[0], current[1]])
+    print(lat)
+    pearlarray.append(lat)
+print(pearlarray[0])
+#a=a[31:]
+#b=b[:-31]
+#print(len(a), len(b))
+#c=list(reader.sortedKeys_DMI[31:1146])
+#plt.plot(c, b, color='r')
+#plt.plot(c, a)
+#plt.show()
+
+
+
+
+
 
